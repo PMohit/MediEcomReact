567e62ee57730491522fee0862cf74a5833139a1
Mohit <mohit.padhye2011@gmail.com>
2020-09-07 12:05:18 +0545

    added Admin login Page

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router} from 'react-router-dom';
-import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router,Route, Switch} from 'react-router-dom';
+import login from './pages/login'
+
 
 ReactDOM.render(
     <Router>
-    <App></App>
+        <Switch>
+             <Route exact path="/" component={login}></Route>
+        </Switch>
     </Router>
-       
     , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ 

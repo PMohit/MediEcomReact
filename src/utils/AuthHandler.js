b4783f3f3e493744821eb92a81af1c45fdeca139
Mohit <mohit.padhye2011@gmail.com>
2020-09-07 13:23:04 +0545

    added login creditionals

---
+++ src/utils/AuthHandler.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+import Config from "./Config";
+import {reactLocalStorage} from 'reactjs-localstorage';
+
+class AuthHandler{
+    static login(username,password,callback){
+        axios
+        .post(Config.loginUrl,{username:username,password:password})
+        .then(function(response){
+              if(response.status === 200){
+                  reactLocalStorage.set("token",response.data.access);
+                  reactLocalStorage.set("refresh",response.data.refresh);
+                  callback({
+                    error:false,
+                    message:"login Successfull"});
+              }
+            //console.log(response);
+        })
+        .catch(function(error){
+            //console.log(error.response);
+            callback({error:true,
+                "message":"Error !! During Login"});
+        });
+    }
+}
+
+export default AuthHandler;

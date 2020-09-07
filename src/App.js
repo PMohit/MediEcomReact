e3a2cb1e8b1eb92279ce7c62c40391c63c366530
Mohit <mohit.padhye2011@gmail.com>
2020-09-07 11:36:04 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React,{Component} from 'react';
+import {Switch, Route} from 
+'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+///import 'bootstrap/dist/css/bootstrap.min.css'
+import { render } from 'react-dom';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
+
+class App extends Component {
+ render(){
+  return (
+     
+      <React.Fragment>
+
+             <Navbar/>
+             <Switch>
+                
+                <Route path="/details" component={Details}></Route>
+                <Route path="/cart" component={Cart}></Route>
+                <Route exact path="/" component={ProductList}></Route>
+                <Route  component={Default}></Route>
+             </Switch>
+              
+      
+
+      </React.Fragment>
+
+       
+
+     );
+  }
+}
+
+export default App;
